af576d7ebda5d417f12eea90a218ec3b0816c106
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-06-29 17:54:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+var str = "HelloWorld";
+
+for (int i = 1; i <= str.Length; i++)
+{
+    for (int j = 0; j <= str.Length - i; j++)
+    {
+        var substr = str.Substring(j, i);
+        Console.WriteLine(substr);
+
+    }
+
+}
